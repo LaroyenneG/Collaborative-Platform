95b0446cea250fc2152d1ab96076656c9064ef3a
laroyenneg <laroyenneg@gmail.com>
2019-10-13 18:00:32 +0200

    swing invocation

--- src/collaborative/platform/agents/CustomerAgent.java
+++ src/collaborative/platform/agents/CustomerAgent.java
@@ -5,13 +5,28 @@ import collaborative.platform.gui.CustomerGUI;
 import jade.gui.GuiAgent;
 import jade.gui.GuiEvent;
 
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+
 public class CustomerAgent extends GuiAgent {
 
     private transient CustomerGUI customerGUI;
 
+
+    private void createGUI() {
+        customerGUI = new CustomerGUI(this);
+    }
+
     @Override
     protected void setup() {
-        customerGUI = new CustomerGUI(this);
+
+        try {
+            SwingUtilities.invokeAndWait(this::createGUI);
+        } catch (InterruptedException | InvocationTargetException e) {
+            customerGUI = null;
+            e.printStackTrace();
+        }
+
         System.out.println("Agent " + getAID().toString() + " started");
     }
 

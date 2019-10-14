fe56f1e03ef8999e122bd8f3d99b795dfcf84732
Adrien Schmuck <adrien.schmuck@uha.fr>
2019-10-14 16:42:17 +0200

    DeliveryAgent: Referencing DF

--- src/collaborative/platform/agents/DeliveryAgent.java
+++ src/collaborative/platform/agents/DeliveryAgent.java
@@ -1,11 +1,27 @@
 package collaborative.platform.agents;
 
-import jade.gui.GuiAgent;
-import jade.gui.GuiEvent;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
-public class DeliveryAgent extends GuiAgent {
+public class DeliveryAgent extends Agent {
     @Override
-    protected void onGuiEvent(GuiEvent guiEvent) {
+    protected void setup() {
+        try {
+            DFAgentDescription dfAgentDescription = new DFAgentDescription();
+            dfAgentDescription.setName(this.getAID());
 
+            ServiceDescription serviceDescription = new ServiceDescription();
+            serviceDescription.setName(getLocalName());
+            serviceDescription.setType(Protocol.SERVICE_DELIVERY);
+
+            dfAgentDescription.addServices(serviceDescription);
+
+            DFService.register(this, dfAgentDescription);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 }

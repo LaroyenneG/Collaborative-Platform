2187a01cf054c438f6fdcf1797ae2d83f8f0016a
LaroyenneG <laroyenneg@gmail.com>
2019-10-14 21:57:37 +0200

    very important commit

--- src/collaborative/platform/gui/CustomerGUI.java
+++ src/collaborative/platform/gui/CustomerGUI.java
@@ -2,7 +2,11 @@ package collaborative.platform.gui;
 
 import collaborative.platform.agents.CustomerAgent;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class CustomerGUI extends JFrame {
 
@@ -15,8 +19,19 @@ public class CustomerGUI extends JFrame {
 
     private void initComponent() {
         setTitle(customerAgent.getLocalName());
-        setSize(300, 300);
+        setSize(500, 500);
         customerAgent.postGuiEvent(null);
+
+        setContentPane(new JPanel() {
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                try {
+                    g.drawImage(ImageIO.read(new URL("https://media.licdn.com/dms/image/C4D03AQEPcYWTlHKSSA/profile-displayphoto-shrink_800_800/0?e=1576713600&v=beta&t=-19dMEZXWhxKvhVgtN4mlQW2bNmD92xK71H7J9B7rBM")), 0, 0, null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public CustomerAgent getCustomerAgent() {

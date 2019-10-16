6ede51a1af53e03a01c82865a39e9447a6a8c80a
Paul <paul.lemettre77@gmail.com>
2019-10-16 13:20:18 +0200

    checkTransaction

--- src/main/java/collaborative/platform/agents/BankerAgent.java
+++ src/main/java/collaborative/platform/agents/BankerAgent.java
@@ -1,12 +1,17 @@
 package collaborative.platform.agents;
 
+import collaborative.platform.behaviors.AcceptationBankTransactionBehaviour;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BankerAgent extends Agent {
+    private Map<String,Long> account;
     protected void setup() {
         DFAgentDescription dfAgentDescription = new DFAgentDescription();
         dfAgentDescription.setName(this.getAID());
@@ -14,6 +19,24 @@ public class BankerAgent extends Agent {
         ServiceDescription serviceDescription = new ServiceDescription();
         serviceDescription.setName(getLocalName());
         serviceDescription.setType(Protocol.SERVICE_BANKER);
+
+        dfAgentDescription.addServices(serviceDescription);
+
+        try {
+            DFService.register(this, dfAgentDescription);
+        } catch (FIPAException e1) {
+            e1.printStackTrace();
+        }
+        account = new HashMap<>();
+        account.put("Customer-A", 1000L);
+        account.put("Customer-B", 500L);
+        account.put("Customer-C",50L);
+
+        addBehaviour(new AcceptationBankTransactionBehaviour(this));
+
+
+
+
     }
 
     @Override
@@ -24,6 +47,10 @@ public class BankerAgent extends Agent {
             e.printStackTrace();
         }
 
+    }
+
+    public Map<String, Long> getAccount(){
+        return account;
     }
 
 }
--- src/main/java/collaborative/platform/behaviors/AcceptationBankTransactionBehaviour.java
+++ src/main/java/collaborative/platform/behaviors/AcceptationBankTransactionBehaviour.java
@@ -1,15 +1,26 @@
 package collaborative.platform.behaviors;
 
+import collaborative.platform.agents.BankerAgent;
 import collaborative.platform.agents.Protocol;
+import collaborative.platform.model.BankTransaction;
+import com.sun.tools.javac.util.Pair;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+
+import static javax.sound.midi.MidiSystem.getReceiver;
 
 public class AcceptationBankTransactionBehaviour extends CyclicBehaviour {
-    public AcceptationBankTransactionBehaviour(Agent a) {
+    public AcceptationBankTransactionBehaviour(BankerAgent a) {
         super(a);
     }
 
+
+    public BankerAgent myAgent(){
+        return (BankerAgent) this.myAgent;
+    }
 
     @Override
     public void action() {
@@ -24,12 +35,49 @@ public class AcceptationBankTransactionBehaviour extends CyclicBehaviour {
     private void messageProcessing(ACLMessage aclMessage) {
         switch (aclMessage.getProtocol()){
             case Protocol.BANKER_ASK_TRANSACTION:
-                checkIfTransactionPossible(aclMessage);
+                boolean possible = checkIfTransactionPossible(aclMessage);
+                /*new
+                ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
+                message.setContentObject(possible);
+                message.setProtocol(Protocol.);
+*/
                 break;
         }
     }
 
-    private void checkIfTransactionPossible(ACLMessage aclMessage) {
+    private Pair<Boolean,Long> checkIfTransactionPossible(ACLMessage aclMessage) {
+        String sender = aclMessage.getSender().getLocalName();
+        BankerAgent bankerAgent = myAgent();
+        Long accountSender = bankerAgent.getAccount().get(sender);
+        if (accountSender == null){
+            return new Pair(false,0);
+        } else {
+            try {
+                BankTransaction bankTransaction = (BankTransaction) aclMessage.getContentObject();
+                long value = bankTransaction.getValue();
+                if (accountSender < value){
+                    return new Pair(false,0);
+                }else{
+                    String receiver = bankTransaction.getReceiver().getLocalName();
+                    Long accountReceiver = bankerAgent.getAccount().get(sender);
+                    if (accountReceiver == null){
+                        return new Pair(false,0);
+                    }else {
+                        Long newValueSender = accountSender - value;
+                        Long newValueReceiver = accountReceiver + value;
+                        bankerAgent.getAccount().put(sender,newValueSender);
+                        bankerAgent.getAccount().put(receiver,newValueReceiver);
+                        return new Pair(true,value);
+                    }
+                }
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+                return new Pair(false,0);
+            }
+
+        }
+
+
 
     }
 }
